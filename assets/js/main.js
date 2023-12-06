d38aeb7969857c7d60a816fbaa137dacc09d2330
Masum Billah <mbillahd@gmail.com>
2023-12-06 19:58:26 +0600

    Uncomment gsap animation code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,35 @@
-// var tl = gsap.timeline();
+var tl = gsap.timeline();
 
-// tl.from("nav .logo , .main-menu a, .nav-right a, .nav-right button, nav .nav-right #feedbacCta, #mobileToggle", {
-//     y: -100,
-//     duration: 1,
-//     delay: 0.5,
-//     stagger: 0.1
-// })
+tl.from("nav .logo , .main-menu a, .nav-right a, .nav-right button, nav .nav-right #feedbacCta, #mobileToggle", {
+    y: -100,
+    duration: 1,
+    delay: 0.5,
+    stagger: 0.1
+})
 
-// tl.from(".hContent h1", {
-//     x: -200,
-//     duration: .3,
-//     opacity: 0, 
-//     stagger: 0.2,
-// })
+tl.from(".hContent h1", {
+    x: -200,
+    duration: .3,
+    opacity: 0, 
+    stagger: 0.2,
+})
 
-// tl.from(".animateHeading", {
-//     y:50,
-//     opacity: 0,
-// })
+tl.from(".animateHeading", {
+    y:50,
+    opacity: 0,
+})
 
-// tl.from(".contentBox_front", {
-//     x: 100,
-//     opacity: 0,
-// })
+tl.from(".contentBox_front", {
+    x: 100,
+    opacity: 0,
+})
 
-// tl.from(".contentBox_back", {
-//     scale: 1, 
-//     opacity: 0,
-//     duration: 5,
-//     rotate: -7,
-// });
+tl.from(".contentBox_back", {
+    scale: 1, 
+    opacity: 0,
+    duration: 5,
+    rotate: -7,
+});
 
 let data = [
     {
